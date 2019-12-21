9a1abc2db43ff9ab485242b910751fa48885d253
lujiliang <lujiliang@aibank.com>
2019-12-21 22:08:38 +0800

    修改注释

--- src/main/java/com/masterlu/code/algorithm/Solution.java
+++ src/main/java/com/masterlu/code/algorithm/Solution.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 
 /**
- * @author lujiliang[lujiliang@aibank.com]
+ * @author masterlu
  * @Description: TODO
  * @date 2019/12/21
  */
@@ -15,9 +15,55 @@ public class Solution {
 
     public static void main(String[] args) {
 
+        //问题1
         int[] ints = twoSum(new int[]{2, 7, 11, 15}, 9);
         print(ints);
 
+        //问题2
+
+    }
+
+    /**
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * 示例：
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        int num = 123;
+
+        return createListNode(num);
+    }
+
+    private static ListNode createListNode(int num) {
+        String numStr = String.valueOf(num);
+        char[] chars = numStr.toCharArray();
+
+        ListNode listNode = new ListNode(Integer.parseInt(String.valueOf(chars[chars.length - 1])));
+        for (int i = chars.length - 2; i > 0; i--) {
+            listNode.next = new ListNode(Integer.parseInt(String.valueOf(chars[i])));
+        }
+
+        return listNode;
+    }
+
+
+    //Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 
     /**
@@ -27,6 +73,7 @@ public class Solution {
      * 给定 nums = [2, 7, 11, 15], target = 9
      * 因为 nums[0] + nums[1] = 2 + 7 = 9
      * 所以返回 [0, 1]
+     *
      * @param nums   nums = [2, 7, 11, 15]
      * @param target target = 9
      * @return
@@ -54,13 +101,15 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 array[1] = i;
-                array[0] = map.get(target-nums[i]);
+                array[0] = map.get(target - nums[i]);
             }
-            map.put(nums[i],i);
+            map.put(nums[i], i);
         }
 
         return array;
 
 
     }
+
+
 }
